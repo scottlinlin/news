95e0c62227b36f925d3352e453423fb4d011aa31
林凤鸣 <>
2019-11-27 23:22:38 +0800

    init

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,12 +1,17 @@
 <template>
 	<view class="content">
+		<!-- <view class="customize-view">
+			<text style="width: 100%;">2019年11月27号</text>
+			<text class="d-right">自定义查询</text>
+		</view> -->
 		<view class="uni-list">
 			<view class="uni-list-cell" hover-class="uni-list-cell-hover" v-for="(item,index) in news" :key="index">
 				<view class="uni-media-list">
-					<image class="uni-media-list-logo" :src="item.author_avatar"></image>
+					<!-- <image class="uni-media-list-logo" :src="item.author_avatar"></image> -->
 					<view class="uni-media-list-body">
 						<view class="uni-media-list-text-top">{{item.title}}</view>
 						<view class="uni-media-list-text-bottom uni-ellipsis">{{item.created_at}}</view>
+						<text class="d-right">12</text>
 					</view>
 				</view>
 			</view>
@@ -42,5 +47,10 @@
 
 <style>
 .uni-media-list-body{height: auto;}
-.uni-media-list-text-top{line-height:1.6em;}	
+.uni-media-list-text-top{line-height:1.6em;}
+.d-right {
+	text-align: right;
+	width: 100%;
+	color: blue;
+}
 </style>
